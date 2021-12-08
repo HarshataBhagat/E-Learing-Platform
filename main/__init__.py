1218d94e4cbe36376075979860d5a9ea7ae0a0c0
HarshataBhagat <harshatahb.b13@gmail.com>
2021-12-08 12:00:16 +0530

    second commite

--- main/__init__.py
+++ main/__init__.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 
 
-app.secret_key = 'thisismysecretkeydonotstealitokay'
+app.secret_key = 'keyhere'
 csrf = CSRFProtect(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = "Database"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
